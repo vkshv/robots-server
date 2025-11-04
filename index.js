eba1e8f37a596e2790976815e0281b2988e22519
Владимир Косоухов <vladimirkosouhov@Vladimirs-MacBook-Pro.local>
2025-11-04 19:04:13 +0300

    add config

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const { STATUS_CODE, STATUS_TEXT } = require('./const/http')
+const { PORT } = require('./config')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`\x1b[42mrobots-server listening on port ${PORT}\x1b[0m`)
 })
