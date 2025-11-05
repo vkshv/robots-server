cd7d740fa6ba28d938ddab56713e72280e6a1b09
Владимир Косоухов <vladimirkosouhov@Vladimirs-MacBook-Pro.local>
2025-11-05 22:42:48 +0300

    pretty start log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+
 const { STATUS_CODE, STATUS_TEXT } = require('./const/http')
 const { PORT } = require('./config')
 
@@ -9,5 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.listen(PORT, () => {
-  console.log(`\x1b[42mrobots-server listening on port ${PORT}\x1b[0m`)
+  console.log(`\x1b[32m╔═══════════════════════════════════════════╗
+║    Server started ${new Date().toLocaleString('ru-RU')}    ║
+╚═══════════════════════════════════════════╝\x1b[0m`)
 })
