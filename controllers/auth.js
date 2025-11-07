a2a0ad1689cad146868a2404cb6bb3455f80ee3c
Косоухов Владимир <vkosoyhov@radiofid.local>
2025-11-07 14:12:03 +0300

    auth wip

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,27 @@
 const jsonwebtoken = require('jsonwebtoken')
 const { JWT_SALT, JWT_REG_SALT } = require('../config')
+const { setCode, getCode, deleteCode } = require('../store')
 
 const requestCode = async function(req, res) {
   const phone = req.body.phone
+
   if (/^[7]\d{10}$/.test(phone) === false) {
     return res.status(400).json({ message: 'Bad Request' })
   }
 
-  // 
+  const prevCode = getCode(phone)
+  if (prevCode) {
+    if (Date.now() - prevCode.createdAt < 60000) {
+      return res.status(429).json({ message: 'Too Many Requests' })
+    }
+  }
 
-  const token = jsonwebtoken.sign({}, JWT_REG_SALT, { expiresIn: '1h' })
+  // отправляем код подтверждения
+  const code = '1111'
+
+  setCode(phone, code)
+
+  const token = jsonwebtoken.sign({}, JWT_REG_SALT, { expiresIn: '10m' })
 }
 
 module.exports = {
--- store/index.js
+++ store/index.js
@@ -1,4 +1,22 @@
 const authCodes = new Map()
 
-const setCode = function(phone, code) {
+const setCode = function(phone, code, lifetime = 900000) { // to prevent verification code from expiring before auth token
+  authCodes.set(phone, { code, createdAt: Date.now() })
+  setTimeout(() => {
+    authCodes.delete(phone)
+  }, lifetime)
+}
+
+const getCode = function(phone) {
+  return authCodes.get(phone)
+}
+
+const deleteCode = function(phone) {
+  authCodes.delete(phone)
+}
+
+module.exports = {
+  setCode,
+  getCode,
+  deleteCode
 }
